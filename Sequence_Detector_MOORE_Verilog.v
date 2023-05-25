8239249a9af22a6d1b6869c4e8c346bd7d7ef56d
amg-eng <101107538+amg-eng@users.noreply.github.com>
2023-05-25 17:32:17 +0300

    Add files via upload

---
+++ Sequence_Detector_MOORE_Verilog.v
@@ -0,0 +1,68 @@
+module Sequence_Detector_MOORE_Verilog(sequence_in,clock,reset,detector_out
+    );
+input clock;
+input reset; 
+input sequence_in; 
+output reg detector_out; 
+parameter  Zero=4'b0000, 
+  One=4'b0001, 
+  OneZero=4'b0010, 
+  OneZeroOne=4'b0101, 
+  OneZeroOneOne=4'b1011;
+reg [3:0] current_state, next_state; 
+always @(posedge clock, posedge reset)
+begin
+ if(reset==1) 
+ current_state <= Zero;// when reset=1, reset the state of the FSM to "Zero" State
+ else
+ current_state <= next_state; // otherwise, next state
+end 
+
+always @(current_state,sequence_in)
+begin
+ case(current_state) 
+ Zero:begin
+  if(sequence_in==1)
+   next_state = One;
+  else
+   next_state = Zero;
+ end
+ One:begin
+  if(sequence_in==0)
+   next_state = OneZero;
+  else
+   next_state = One;
+ end
+ OneZero:begin
+  if(sequence_in==0)
+   next_state = Zero;
+  else
+   next_state = OneZeroOne;
+ end 
+ OneZeroOne:begin
+  if(sequence_in==0)
+   next_state = OneZero;
+  else
+   next_state = OneZeroOneOne;
+ end
+ OneZeroOneOne:begin
+  if(sequence_in==0)
+   next_state = OneZero;
+  else
+   next_state = One;
+ end
+ default:next_state = Zero;
+ endcase
+end
+always @(current_state)
+begin 
+ case(current_state) 
+ Zero:   detector_out = 0;
+ One:   detector_out = 0;
+ OneZero:  detector_out = 0;
+ OneZeroOne:  detector_out = 0;
+ OneZeroOneOne:  detector_out = 1;
+ default:  detector_out = 0;
+ endcase
+end 
+endmodule
